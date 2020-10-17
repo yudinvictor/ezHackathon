31c19c8f0dbebae08ac303e61771318c76452cdf
yudinvictor <vayudin_1@edu.hse.ru>
2020-10-18 00:19:12 +0300

    plan page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MainLayoutComponent} from "./main-layout/main-layout.component";
 import {LandPageComponent} from "./land-page/land-page.component";
+import {PlanPageComponent} from "./plan-page/plan-page.component";
 
 
 const routes: Routes = [
   {path: '', component: MainLayoutComponent, children: [
       {path: '', redirectTo: '/land', pathMatch: 'full'},
       {path: 'land', component: LandPageComponent},
+      {path: 'plan', component: PlanPageComponent},
     ]
   },
 ];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MaterialModule} from "./material-module";
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { LandPageComponent } from './land-page/land-page.component';
+import { PlanPageComponent } from './plan-page/plan-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
-    LandPageComponent
+    LandPageComponent,
+    PlanPageComponent
   ],
   imports: [
     BrowserModule,
