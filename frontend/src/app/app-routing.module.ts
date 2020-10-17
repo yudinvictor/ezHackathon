85489f9bffec0e0eb28825ca56ede97d5102b7ba
yudinvictor <vayudin_1@edu.hse.ru>
2020-10-17 22:29:48 +0300

    land

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MainLayoutComponent} from "./main-layout/main-layout.component";
+import {LandPageComponent} from "./land-page/land-page.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainLayoutComponent, children: [
+      {path: '', redirectTo: '/land', pathMatch: 'full'},
+      {path: 'land', component: LandPageComponent},
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from "./material-module";
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { LandPageComponent } from './land-page/land-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    LandPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
