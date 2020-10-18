abc6c3d8894cfe050aa92b87f4c7e595adeb2b24
mirabu <bober2801@gmail.com>
2020-10-18 06:11:19 +0300

    fix

--- frontend/src/app/services/nodes.service.ts
+++ frontend/src/app/services/nodes.service.ts
@@ -5,28 +5,40 @@ import {ApiService} from './api.service';
   providedIn: 'root'
 })
 export class NodesService {
-  optimizeOptions: any = {penalty: [[0, 0]], 0: [[0, 0, 0, 0, 0, 0, 0]]};
+  optimizeOptions: any = {
+    penalty: [[0, 0]],
+    0: [[0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
+  };
   changes = [];
 
   constructor(private apiService: ApiService) {
   }
 
-  addChanges(id, start_date, duration) {
+  addChanges(id, start_date, duration, onDone = null) {
     const change = {
       id,
       start_date,
       duration,
     };
     this.changes.push(change);
-    this.getGraph(change);
+    this.getGraph(change, onDone);
   }
 
-  getGraph(change) {
+  getGraph(change, onDone = null) {
     console.log(change);
+    if (change == null) {
+      this.apiService.getData().subscribe(value1 => {
+        this.optimizeOptions = value1;
+        onDone(value1);
+      });
+      return;
+    }
     this.apiService.change(change).subscribe((value: any) => {
       console.log(value);
       this.apiService.getData().subscribe(value1 => {
         this.optimizeOptions = value1;
+        onDone(value1);
       });
     });
   }
